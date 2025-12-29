ca5f69cf794afb87b35b665ece3f3085e3aebec6
Lukas Peters <peterslukas0512@gmail.com>
2025-12-28 22:35:24 -0500

    proxmox doesnt use sudo

--- samples/ubuntu/ubuntu-noble-cloudinit.sh
+++ samples/ubuntu/ubuntu-noble-cloudinit.sh
@@ -34,20 +34,20 @@ rm -f $IMG_NOIMG-resized.img
 cp $IMG $IMG_NOIMG-resized.img
 qemu-img resize $IMG_NOIMG-resized.img 8G
 
-sudo qm destroy $VMID || true
-sudo qm create $VMID --name $NAME --ostype l26 \
+qm destroy $VMID || true
+qm create $VMID --name $NAME --ostype l26 \
     --memory 1024 --balloon 0 \
     --agent 1 \
     --bios ovmf --machine q35 --efidisk0 $STORAGE:0,pre-enrolled-keys=0 \
     --cpu host --socket 1 --cores 1 \
     --vga serial0 --serial0 socket  \
     --net0 virtio,bridge=vmbr0
-sudo qm importdisk $VMID noble-server-cloudimg-amd64-resized.img $STORAGE
-sudo qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
-sudo qm set $VMID --boot order=virtio0
-sudo qm set $VMID --scsi1 $STORAGE:cloudinit
+qm importdisk $VMID noble-server-cloudimg-amd64-resized.img $STORAGE
+qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
+qm set $VMID --boot order=virtio0
+qm set $VMID --scsi1 $STORAGE:cloudinit
 
-cat << EOF | sudo tee /var/lib/vz/snippets/ubuntu.yaml
+cat << EOF | tee /var/lib/vz/snippets/ubuntu.yaml
 #cloud-config
 runcmd:
     - apt-get update
@@ -57,9 +57,9 @@ runcmd:
 # Taken from https://forum.proxmox.com/threads/combining-custom-cloud-init-with-auto-generated.59008/page-3#post-428772
 EOF
 
-sudo qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
-sudo qm set $VMID --tags ubuntu-template,noble,cloudinit
-sudo qm set $VMID --ciuser $USER
-sudo qm set $VMID --sshkeys ~/.ssh/authorized_keys
-sudo qm set $VMID --ipconfig0 ip=dhcp
-sudo qm template $VMID
+qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
+qm set $VMID --tags ubuntu-template,noble,cloudinit
+qm set $VMID --ciuser $USER
+qm set $VMID --sshkeys ~/.ssh/authorized_keys
+qm set $VMID --ipconfig0 ip=dhcp
+qm template $VMID
