faa4d2634b8328c474572c4657180446fab4751c
Lukas Peters <peterslukas0512@gmail.com>
2025-12-28 22:38:05 -0500

    update name

--- samples/ubuntu/ubuntu-noble-cloudinit.sh
+++ samples/ubuntu/ubuntu-noble-cloudinit.sh
@@ -42,7 +42,7 @@ qm create $VMID --name $NAME --ostype l26 \
     --cpu host --socket 1 --cores 1 \
     --vga serial0 --serial0 socket  \
     --net0 virtio,bridge=vmbr0
-qm importdisk $VMID noble-server-cloudimg-amd64-resized.img $STORAGE
+qm importdisk $VMID $IMG_NOIMG-resized.img $STORAGE
 qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
 qm set $VMID --boot order=virtio0
 qm set $VMID --scsi1 $STORAGE:cloudinit
@@ -58,8 +58,8 @@ runcmd:
 EOF
 
 qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
-qm set $VMID --tags ubuntu-template,noble,cloudinit
+qm set $VMID --tags ubuntu-template,plucky,cloudinit
 qm set $VMID --ciuser $USER
-qm set $VMID --sshkeys ~/.ssh/authorized_keys
+# qm set $VMID --sshkeys ~/.ssh/authorized_keys
 qm set $VMID --ipconfig0 ip=dhcp
 qm template $VMID
